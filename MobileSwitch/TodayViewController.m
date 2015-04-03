b80a20760b2db7ab45cb3eecea55980af4fcb134
gongliang <gongliang@aprbrother.com>
2015-04-03 11:33:46 +0800

    bounds

--- MobileSwitch/TodayViewController.m
+++ MobileSwitch/TodayViewController.m
@@ -12,7 +12,7 @@
 #import "GLBluetoothController.h"
 
 
-static CGFloat kDefaultHeight = 150.0f;
+static CGFloat kDefaultHeight = 25.0f;
 
 extern BOOL CTCellularDataPlanGetIsEnabled();
 extern void CTCellularDataPlanSetIsEnabled(BOOL enabled);
@@ -30,10 +30,16 @@ extern void CTCellularDataPlanSetIsEnabled(BOOL enabled);
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    self.preferredContentSize = CGSizeMake(0, kDefaultHeight );
+    NSLog(@"self.view = %@", self.view);
+//    self.preferredContentSize = CGSizeMake(0, kDefaultHeight);
+    [self.view bringSubviewToFront:self.mobileDataSwitch];
     self.mobileDataSwitch.on = CTCellularDataPlanGetIsEnabled();
     _btCont = [GLBluetoothController sharedInstance];
     _blueToothSwitch.on = _btCont.enabled;
+}
+
+- (UIEdgeInsets)widgetMarginInsetsForProposedMarginInsets:(UIEdgeInsets)defaultMarginInsets {
+    return UIEdgeInsetsMake(10, 20, -10, 0);
 }
 
 - (void)didReceiveMemoryWarning {
